70ea15f49cc9e71e420fbb2562cadd4e8c34bd58
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-10-23 02:44:20 -0200

    :sparkles: Addinng animation to elements

---
+++ js/main.js
@@ -0,0 +1,18 @@
+Visibility.onVisible(function(){
+  // Index
+  setTimeout(function(){
+    $('.header a').addClass('animated fadeInDown')
+  }, 400)  
+  setTimeout(function(){
+    $('.header nav').addClass('animated fadeInDown')
+  }, 400)
+  setTimeout(function(){
+    $('.introduction').addClass('animated fadeInDown')
+  }, 800)
+  setTimeout(function(){
+    $('.footer-media ul').addClass('animated fadeInDown')
+  }, 1200)
+  setTimeout(function(){
+    $('.footer-copy span').addClass('animated fadeInDown')
+  }, 1600)
+})
